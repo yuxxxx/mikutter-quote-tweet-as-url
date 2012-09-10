49f550f544f0f495ebfc55818372b1299e7e1007
yuxxxx <marlcabbage@gmail.com>
2012-09-11 07:09:07 +0900

    複数ツイートの引用に対応 	modified:   quote_as_tweet_url.rb

--- quote_as_tweet_url.rb
+++ quote_as_tweet_url.rb
@@ -1,20 +1,25 @@
 # -*- coding: utf-8 -*-
-Plugin.create :quote_as_tweet_url do
+Plugin.create :quote_tweet_as_url do
+  is_quotable = Proc.new {|message|
+    message.message.retweetable? || message.message.from_me? 
+  }
   filter_command do |menu|
-    menu[:quote_as_tweet_url] = {
-      :slug => :quote_as_tweet_url,
+    menu[:quote_tweet_as_url] = {
+      :slug => :quote_tweet_as_url,
       :name => 'ツイートのURLを引用',
-      :condition => lambda{ |m| m.message.retweetable? },
-      :exec => lambda{ |m|
+      :condition => lambda{ |messages| messages.any?(&is_quotable) },
+      :exec => lambda{ |messages|
         postboxes = Plugin.filtering(:main_postbox, nil).first
         postbox = Gtk::PostBox.new(Post.primary_service,
                                    postboxstorage: postboxes)
         widget = postbox.widget_post
         postboxes.pack_start(postbox).show_all.get_ancestor(Gtk::Window).set_focus(widget)
-        widget.buffer.text = "https://twitter.com/#!/#{m.message.user[:idname]}/statuses/#{m.message[:id]}"
+        messages.keep_if(&is_quotable).each{ |quoted_message|
+          widget.buffer.text += "http://twitter.com/#{quoted_message.message[:user][:idname]}/statuses/#{quoted_message.message[:id]} "
+        }
       },
       :visible => true,
-      :role => :message }
+      :role => :messages }
     [menu]
   end
 end
